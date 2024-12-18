122d52b93ac742de89d74b39e13851c394c7d847
Evgeny Svirsky <e.svirsky@tut.by>
2024-12-18 14:29:39 +0100

    - Updated README

--- packages/___sdk/src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const MAINNET_API_URL = "https://api.hyperliquid.xyz"
-export const TESTNET_API_URL = "https://api.hyperliquid-testnet.xyz"
-export const LOCAL_API_URL = "http://localhost:3001"
