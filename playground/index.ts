3cd29efda333e04f9c7794e1b95f8b2fceb88acb
Evgeny Svirsky <e.svirsky@tut.by>
2024-12-19 10:24:14 +0100

    - Minor fixes + tests uncomment

--- playground/index.ts
+++ playground/index.ts
@@ -1,11 +1,14 @@
-import { testInfoAPI } from './tests';
+import {testInfoAPI, testPerpetualsInfoAPI, testSpotInfoAPI} from './tests';
 import { Hyperliquid } from 'hyperliquid-sdk';
 
 async function main() {
   const sdk = new Hyperliquid('');
   await testInfoAPI(sdk);
-  // await testSpotInfoAPI(sdk);
 
-  // await testPerpetualsInfoAPI(sdk);
+  await testSpotInfoAPI(sdk);
+
+  await testPerpetualsInfoAPI(sdk);
+
+  process.exit(0);
 }
 main();
--- playground/tests.ts
+++ playground/tests.ts
@@ -1,85 +1,78 @@
-import { Hyperliquid } from '@hyperliquid-sdk/sdk';
+import { Hyperliquid } from 'hyperliquid-sdk';
 
-const raw_mode = true;
-const user_address = '';
+const user_address = '0x8998a54320cD3Ec83F1837F7E2A9640e282e6711';
 
 export async function testInfoAPI(sdk: Hyperliquid) {
   console.log('Testing InfoAPI methods:');
 
   console.log('getAllMids:');
-  console.log(await sdk.info.getAllMids(raw_mode));
+  console.log(await sdk.info.getAllMids());
 
-  // console.log('getUserOpenOrders:');
-  // console.log(await sdk.info.getUserOpenOrders(user_address, raw_mode));
-  //
-  // console.log('getFrontendOpenOrders:');
-  // console.log(await sdk.info.getFrontendOpenOrders(user_address, raw_mode));
-  //
-  // console.log('getUserFills:');
-  // console.log(await sdk.info.getUserFills(user_address, raw_mode));
-  //
-  // console.log('getUserFillsByTime:');
-  // console.log(
-  //   await sdk.info.getUserFillsByTime(
-  //     user_address,
-  //     Date.now() - 1506400000,
-  //     Date.now(),
-  //     raw_mode,
-  //   ),
-  // ); // Last 24 hours
-  //
-  // console.log('getUserRateLimit:');
-  // console.log(await sdk.info.getUserRateLimit(user_address, raw_mode));
-  //
-  // console.log('getOrderStatus:');
-  // console.log(await sdk.info.getOrderStatus(user_address, 1000, raw_mode));
-  //
-  // console.log('getL2Book:');
-  // const book = await sdk.info.getL2Book('BTC-PERP', raw_mode);
-  // console.log(book);
-  // console.log(book.levels);
-  //
-  // console.log('getCandleSnapshot:');
-  // console.log(
-  //   await sdk.info.getCandleSnapshot(
-  //     'BTC-PERP',
-  //     '1h',
-  //     Date.now() - 86400000,
-  //     Date.now(),
-  //     raw_mode,
-  //   ),
-  // );
+  console.log('getUserOpenOrders:');
+  console.log(await sdk.info.getUserOpenOrders(user_address));
+
+  console.log('getFrontendOpenOrders:');
+  console.log(await sdk.info.getFrontendOpenOrders(user_address));
+
+  console.log('getUserFills:');
+  console.log(await sdk.info.getUserFills(user_address));
+
+  console.log('getUserFillsByTime:');
+  console.log(
+    await sdk.info.getUserFillsByTime(
+      user_address,
+      Date.now() - 1506400000,
+      Date.now(),
+    ),
+  ); // Last 24 hours
+
+  console.log('getUserRateLimit:');
+  console.log(await sdk.info.getUserRateLimit(user_address));
+
+  console.log('getOrderStatus:');
+  console.log(await sdk.info.getOrderStatus(user_address, 1000));
+
+  console.log('getL2Book:');
+  const book = await sdk.info.getL2Book('BTC-PERP');
+  console.log(book);
+  console.log(book.levels);
+
+  console.log('getCandleSnapshot:');
+  console.log(
+    await sdk.info.getCandleSnapshot(
+      'BTC-PERP',
+      '1h',
+      Date.now() - 86400000,
+      Date.now(),
+    ),
+  );
 }
 
 export async function testSpotInfoAPI(sdk: Hyperliquid) {
   console.log('\nTesting SpotInfoAPI methods:');
 
   console.log('getSpotMeta:');
-  console.log(await sdk.info.spot.getSpotMeta(raw_mode));
+  console.log(await sdk.info.spot.getSpotMeta());
 
   console.log('getSpotClearinghouseState:');
-  console.log(
-    await sdk.info.spot.getSpotClearinghouseState(user_address, raw_mode),
-  );
+  console.log(await sdk.info.spot.getSpotClearinghouseState(user_address));
 
   console.log('getSpotMetaAndAssetCtxs:');
-  console.log(await sdk.info.spot.getSpotMetaAndAssetCtxs(raw_mode));
+  console.log(await sdk.info.spot.getSpotMetaAndAssetCtxs());
 }
 
 export async function testPerpetualsInfoAPI(sdk: Hyperliquid) {
   console.log('\nTesting PerpetualsInfoAPI methods:');
 
   console.log('getMeta:');
-  console.log(await sdk.info.perpetuals.getMeta(raw_mode));
+  console.log(await sdk.info.perpetuals.getMeta());
 
   console.log('getMetaAndAssetCtxs:');
-  console.log(await sdk.info.perpetuals.getMetaAndAssetCtxs(raw_mode));
+  console.log(await sdk.info.perpetuals.getMetaAndAssetCtxs());
 
   console.log('getClearinghouseState:');
-  const perpsClearing = await sdk.info.perpetuals.getClearinghouseState(
-    user_address,
-    raw_mode,
-  );
+  const perpsClearing =
+    await sdk.info.perpetuals.getClearinghouseState(user_address);
   console.log(perpsClearing);
   console.log(perpsClearing.assetPositions);
 
@@ -89,7 +82,6 @@ export async function testPerpetualsInfoAPI(sdk: Hyperliquid) {
       user_address,
       Date.now() - 86400000,
       Date.now(),
-      raw_mode,
     ),
   );
 
@@ -99,7 +91,6 @@ export async function testPerpetualsInfoAPI(sdk: Hyperliquid) {
       user_address,
       Date.now() - 86400000,
       Date.now(),
-      raw_mode,
     ),
   );
 
@@ -109,7 +100,6 @@ export async function testPerpetualsInfoAPI(sdk: Hyperliquid) {
       'BTC-PERP',
       Date.now() - 86400000,
       Date.now(),
-      raw_mode,
     ),
   );
 }
