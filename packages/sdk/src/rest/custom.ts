d81d4a0159448248d4d2d32c7a5e29ba12ac1df5
Evgeny Svirsky <e.svirsky@tut.by>
2025-01-03 18:37:52 +0100

    - Fixed decimal calculation

--- packages/sdk/src/rest/custom.ts
+++ packages/sdk/src/rest/custom.ts
@@ -97,7 +97,7 @@ export class CustomOperations {
     console.log(decimals);
 
     px *= isBuy ? 1 + slippage : 1 - slippage;
-    const spotDecimals = px < 1 ? 6 : 8; // TODO: Check the calculation here: https://github.com/ccxt/ccxt/issues/23516 and here: https://github.com/gaardiolor/ccxt/commit/de43976f408007bf2a8555141b13936735645702#diff-bafe810770b7e09d82844d4f5b0efc778f4e2024f8dfb7973faa9cf603d0ca0eR456
+    const spotDecimals = px < 1 ? 5 : 8; // TODO: Check the calculation here: https://github.com/ccxt/ccxt/issues/23516 and here: https://github.com/gaardiolor/ccxt/commit/de43976f408007bf2a8555141b13936735645702#diff-bafe810770b7e09d82844d4f5b0efc778f4e2024f8dfb7973faa9cf603d0ca0eR456
     return Number(px.toFixed(isSpot ? spotDecimals : decimals - 1));
   }
 
